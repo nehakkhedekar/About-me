d08d067a0b0ad4e2dbba39c821a3d981b95ac2c0
Neha Khedekar <khedekarneha30@gmail.com>
2023-07-28 13:24:35 -0400

    Initial Commit

---
+++ About me/About_meApp.swift
@@ -0,0 +1,17 @@
+//
+//  About_meApp.swift
+//  About me
+//
+//  Created by Neha Khedekar on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct About_meApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ About me/ContentView.swift
@@ -0,0 +1,38 @@
+//
+//  ContentView.swift
+//  About me
+//
+//  Created by Neha Khedekar on 7/27/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var fact1 = ""
+    @State private var fact2 = ""
+    @State private var fact3 = ""
+    
+    var body: some View {
+      VStack {
+          Text("Hi, I'm Neha!")
+              .font(.largeTitle)
+          Button("Let me tell you some things about me!"){
+              fact1 = "I have 1 older brother"
+              fact2 = "I loveeee Thai food"
+              fact3 = "Boston is my favorite city!"
+              
+          }
+            
+            }
+            
+        .padding()
+    }
+}
+
+        
+struct ContentView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        ContentView()
+    }
+ }
